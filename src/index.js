4c45ea1b502a56d412e5bffa0fbe296227c2be6f
Ahmad Naeem <ahmadnaeem26831@gmail.com>
2024-09-12 10:57:57 +0500

    add utils code error and response files

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ import dotenv from "dotenv";
 import express from "express";
 import connectDB from "./db/index.js";
 
+const app = express();
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERROR: ", error);
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`MongoDb connection is failed !!! ${err}`);
+  });
